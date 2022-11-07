40b6706fb17dd5a17a0f32868999c9a76d44c2b7
Leprekon <weisong@hotmail.sg>
2022-11-07 20:35:01 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,16 +3,22 @@ let USL = 0
 basic.clearScreen()
 radio.setGroup(1)
 radio.setFrequencyBand(7)
-basic.forever(function on_forever() {
-    
+basic.forever(function () {
     basic.pause(100)
-    USL = sonar.ping(DigitalPin.P1, DigitalPin.P1, PingUnit.Centimeters)
-    USH = sonar.ping(DigitalPin.P0, DigitalPin.P0, PingUnit.Centimeters)
+    USL = sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P1,
+    PingUnit.Centimeters
+    )
+    USH = sonar.ping(
+    DigitalPin.P0,
+    DigitalPin.P0,
+    PingUnit.Centimeters
+    )
     if (USL < 30 && USH > 30) {
         radio.sendValue("USH", USH)
         radio.sendValue("USL", USL)
     } else {
         basic.showIcon(IconNames.Yes)
     }
-    
 })
